7731813c0287fa1b2515f4d9ce56028f84189566
Ergin Dervisoglu <ergin.dervisoglu@docusign.com>
2013-10-30 10:56:30 -0700

    Adding warranties to all source files

--- populate-with-demo-data.js
+++ populate-with-demo-data.js
@@ -1,3 +1,21 @@
+/**
+ * @copyright Copyright (C) DocuSign, Inc.  All rights reserved.
+ *
+ * This source code is intended only as a supplement to DocuSign SDK
+ * and/or on-line documentation.
+ * 
+ * This sample is designed to demonstrate DocuSign features and is not intended
+ * for production use. Code and policy for a production application must be
+ * developed to meet the specific data and security requirements of the
+ * application.
+ *
+ * THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
+ * KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+ * PARTICULAR PURPOSE.
+ */
+
+
 var moment = require('moment');
 var csv = require('csv');
 var db = new (require('sqlite3')).Database('lobby.db');
